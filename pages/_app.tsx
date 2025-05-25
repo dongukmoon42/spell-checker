4a4e8d88e8c5612c3220f632070bc18970ab7ab8
ResoM <dongukmoon42@gmail.com>
2025-05-26 01:20:54 +0900

    chore: add Search Console meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Footer from '@components/Footer';
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="google-site-verification" content="ZO0F_uWh8Y9h0jybKLd4Q8BVaraQ4agZxsEbOI-tec0" />
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
