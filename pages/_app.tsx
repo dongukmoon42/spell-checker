9b8eebbacb42863736876ff88b1bb2f982348898
Prog <dongukmoon42@gmail.com>
2025-05-27 21:36:15 +0900

    "feat: insert AdSense auto ads script"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* Google Search Console 인증 */}
         <meta name="google-site-verification" content="ZO0F_uWh8Y9h0jybKLd4Q8BVaraQ4agZxsEbOI-tec0" />
+
+        {/* ✅ 애드센스 자동 광고 스크립트 */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3136907678966893"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <Component {...pageProps} />
       <Footer />
